7c9ea14b56facf9e91d8c29e9fe76221d7f27594
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2024-01-23 14:37:00 -0300

    Front-End do Nav concluído

--- template.php
+++ template.php
@@ -26,15 +26,14 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://startbootstrap.com">Start Bootstrap</a>
+          <a class="navbar-brand" href="http://startbootstrap.com">CodeSnape</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">Meus Trechos</a></li>
+            <li><a href="#services">Adicionar</a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container -->
