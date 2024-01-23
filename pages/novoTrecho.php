d7045ebac48f730abba26ca46596130ebcb70eb6
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2024-01-23 17:35:13 -0300

    Front-End do Novo Trecho Concluído

--- pages/novoTrecho.php
+++ pages/novoTrecho.php
@@ -1,1 +1,29 @@
-<h1>ADICIONAR</h1>
+<div class="row">
+
+    <div class="col-lg-12">
+        <h1 class="page-header">Adicionar <small>Novo Trecho</small></h1>
+    </div>
+    
+    <form class="col-md-4">
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Nome do Trecho:</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Titulo">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Linguagem</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>HTML</option>
+            <option>CSS</option>
+            <option>PHP</option>
+            <option>JavaScript</option>
+            <option>MySQL</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Trecho</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+</div>
+
