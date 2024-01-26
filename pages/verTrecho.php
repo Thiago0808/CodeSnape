1fbb2160197097e03cf31c2cac572d362c48dea6
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2024-01-25 22:12:31 -0300

    Botão Deletar no verTrecho

--- pages/verTrecho.php
+++ pages/verTrecho.php
@@ -6,6 +6,8 @@
 
     <div class="col-lg-12">
         <code><?=$t->texto?></code>
+        <br>
+        <a href='?p=deletarTrecho&id=<?=$t->id?>'><button type='button' class='btn btn-danger'>Deletar</button></a>
     </div>
 
 </div>
