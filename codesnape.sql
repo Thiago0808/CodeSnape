1624c8fdef8d528961d39f0884de3359df4cb394
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2025-07-10 23:22:58 -0300

    feat: BD atualizado

--- codesnape.sql
+++ codesnape.sql
@@ -3,56 +3,123 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23-Jan-2024 às 00:20
--- Versão do servidor: 10.4.28-MariaDB
--- versão do PHP: 8.2.4
+-- Tempo de geração: 11/07/2025 às 04:21
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- Cria o banco de dados (se não existir) e usa ele
+CREATE DATABASE IF NOT EXISTS `codesnape` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `codesnape`;
 
+-- Configurações de charset
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `codesnape`
---
+-- --------------------------------------------------------
+
+-- Estrutura para tabela `linguagem`
+CREATE TABLE `linguagem` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `trecho_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
---
--- Estrutura da tabela `trecho`
---
+-- Estrutura para tabela `tag`
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  `color` varchar(7) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+-- Estrutura para tabela `tag_trecho`
+CREATE TABLE `tag_trecho` (
+  `tag_id` int(11) NOT NULL,
+  `trecho_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+-- Estrutura para tabela `trecho`
 CREATE TABLE `trecho` (
   `id` int(11) NOT NULL,
   `titulo` varchar(200) NOT NULL,
-  `linguagem` varchar(50) NOT NULL,
-  `texto` text NOT NULL
+  `texto` text NOT NULL,
+  `usuario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
+-- --------------------------------------------------------
 
---
--- Índices para tabela `trecho`
---
+-- Estrutura para tabela `usuario`
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` text NOT NULL,
+  `ativo` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Índices
+
+ALTER TABLE `linguagem`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `trecho_id` (`trecho_id`);
+
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+ALTER TABLE `tag_trecho`
+  ADD PRIMARY KEY (`tag_id`,`trecho_id`),
+  ADD KEY `tag_id` (`tag_id`,`trecho_id`),
+  ADD KEY `trecho_id` (`trecho_id`);
+
 ALTER TABLE `trecho`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
+-- AUTO_INCREMENT
 
---
--- AUTO_INCREMENT de tabela `trecho`
---
+ALTER TABLE `linguagem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+ALTER TABLE `tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
 ALTER TABLE `trecho`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+-- Restrições de chave estrangeira
+
+ALTER TABLE `linguagem`
+  ADD CONSTRAINT `linguagem_ibfk_1` FOREIGN KEY (`trecho_id`) REFERENCES `trecho` (`id`);
+
+ALTER TABLE `tag`
+  ADD CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `tag_trecho`
+  ADD CONSTRAINT `tag_trecho_ibfk_1` FOREIGN KEY (`trecho_id`) REFERENCES `trecho` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tag_trecho_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `trecho`
+  ADD CONSTRAINT `trecho_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
